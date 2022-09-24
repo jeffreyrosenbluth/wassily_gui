c5a13e886d307e39eb79c456b21833ef1599aa5f
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2022-09-24 10:55:38 -0400

    working with scale2

--- src/app.rs
+++ src/app.rs
@@ -1,21 +1,26 @@
+use crate::art::*;
+use egui::{ColorImage, TextureFilter};
+use wassily::prelude::Pixmap;
+
+const WIDTH: u32 = 1000;
+const HEIGHT: u32 = 1000;
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
-    // Example stuff:
-    label: String,
+#[serde(default)]
+// if we add new fields, give them default values when deserializing old state
 
-    // this how you opt-out of serialization of a member
+pub struct TemplateApp {
+    label: String,
     #[serde(skip)]
-    value: f32,
+    art: Art,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
-            // Example stuff:
             label: "Hello World!".to_owned(),
-            value: 2.7,
+            art: Art::new(0.4),
         }
     }
 }
@@ -42,75 +47,73 @@ impl eframe::App for TemplateApp {
         eframe::set_value(storage, eframe::APP_KEY, self);
     }
 
-    /// Called each time the UI needs repainting, which may be many times per second.
-    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let Self { label, value } = self;
-
-        // Examples of how to create different panels and windows.
-        // Pick whichever suits you.
-        // Tip: a good default choice is to just keep the `CentralPanel`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        // let Self {
+        //     label,
+        //     value,
+        //     mut art,
+        // } = self;
+        // let mut art = self.art.clone();
+        let pixmap = draw(WIDTH, HEIGHT, 1.0, &self.art);
 
         #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
             egui::menu::bar(ui, |ui| {
                 ui.menu_button("File", |ui| {
                     if ui.button("Quit").clicked() {
-                        _frame.close();
+                        frame.close();
                     }
                 });
             });
         });
 
         egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
+            ui.heading("Close");
 
             ui.horizontal(|ui| {
                 ui.label("Write something: ");
-                ui.text_edit_singleline(label);
+                ui.text_edit_singleline(&mut self.label);
             });
 
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
+            ui.add(
+                egui::Slider::new(&mut self.art.radial_middle_stop, 0.0..=1.0)
+                    .text("Middle Gradient Stop"),
+            );
+            ui.add_space(20.0);
+            if ui.button("Save").clicked() {
+                print(pixmap.width(), pixmap.height(), 1.08, &self.art);
             }
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to(
-                        "eframe",
-                        "https://github.com/emilk/egui/tree/master/crates/eframe",
-                    );
-                    ui.label(".");
-                });
-            });
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
+            ui.heading("Wassily Gui App");
+            ui.add_space(20.0);
             egui::warn_if_debug_build(ui);
-        });
-
-        if false {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally chose either panels OR windows.");
+            let mut opt_texture: Option<egui::TextureHandle> = None;
+            let texture: &egui::TextureHandle = opt_texture.get_or_insert_with(|| {
+                ui.ctx()
+                    .load_texture("wave", generate(pixmap), TextureFilter::default())
             });
-        }
+            let img_size = 1.0 * texture.size_vec2();
+            ui.horizontal(|ui| {
+                ui.add_space(20.0);
+                ui.add_sized(
+                    [WIDTH as f32, HEIGHT as f32],
+                    egui::Image::new(texture, img_size),
+                );
+            });
+        });
     }
+}
+
+fn generate(pixmap: Pixmap) -> ColorImage {
+    ColorImage::from_rgba_unmultiplied(
+        [pixmap.width() as usize, pixmap.height() as usize],
+        pixmap.data(),
+    )
+}
+
+fn print(width: u32, height: u32, scale: f32, art: &Art) {
+    let pixmap = draw(width, height, scale, art);
+    pixmap.save_png("./output/grad.png");
 }
---
+++ src/art.rs
@@ -0,0 +1,92 @@
+use wassily::prelude::*;
+
+#[derive(Debug, Clone)]
+pub struct Art {
+    pub radial_middle_stop: f32,
+}
+
+impl Art {
+    pub fn new(radial_middle_stop: f32) -> Self {
+        Self { radial_middle_stop }
+    }
+}
+
+pub fn draw(width: u32, height: u32, scale: f32, art: &Art) -> Pixmap {
+    const EDGE: u32 = 250;
+    const PAD: u32 = 100;
+    let mut canvas = Canvas::with_scale(width, height, scale);
+
+    let lg = LinearGradient::new(
+        pt(0.0, 0.0),
+        pt(0.0, canvas.h_f32()),
+        vec![
+            GradientStop::new(0.0, *WHITE),
+            GradientStop::new(1.0, *BLACK),
+        ],
+        SpreadMode::Pad,
+        Transform::identity(),
+    )
+    .unwrap();
+    let linear = paint_shader(lg);
+
+    let rg = RadialGradient::new(
+        pt(canvas.w_f32() / 2.0, canvas.h_f32() / 2.0),
+        pt(canvas.w_f32() / 2.0, canvas.h_f32() / 2.0),
+        700.0,
+        vec![
+            GradientStop::new(0.0, *MAROON),
+            GradientStop::new(art.radial_middle_stop, *ORANGE),
+            GradientStop::new(1.0, *BLUE),
+        ],
+        SpreadMode::Pad,
+        Transform::identity(),
+    )
+    .unwrap();
+    let radial = paint_shader(rg);
+
+    ShapeBuilder::new()
+        .rect_xywh(pt(0, 0), pt(canvas.width, canvas.height))
+        .fill_paint(&linear)
+        .no_stroke()
+        .build()
+        .draw(&mut canvas);
+
+    ShapeBuilder::new()
+        .rect_xywh(
+            pt(
+                canvas.w_f32() / 2.0 - EDGE as f32 / 2.0,
+                canvas.h_f32() / 2.0 - EDGE as f32 / 2.0,
+            ),
+            pt(EDGE, EDGE),
+        )
+        .fill_paint(&radial)
+        .stroke_weight(4.0)
+        .build()
+        .draw(&mut canvas);
+
+    ShapeBuilder::new()
+        .rect_xywh(pt(PAD, PAD), pt(EDGE, EDGE))
+        .fill_paint(&radial)
+        .build()
+        .draw(&mut canvas);
+    ShapeBuilder::new()
+        .rect_xywh(pt(canvas.width() - EDGE - PAD, PAD), pt(EDGE, EDGE))
+        .fill_paint(&radial)
+        .build()
+        .draw(&mut canvas);
+    ShapeBuilder::new()
+        .rect_xywh(pt(PAD, canvas.width() - EDGE - PAD), pt(EDGE, EDGE))
+        .fill_paint(&radial)
+        .build()
+        .draw(&mut canvas);
+    ShapeBuilder::new()
+        .rect_xywh(
+            pt(canvas.width() - EDGE - PAD, canvas.height() - EDGE - PAD),
+            pt(EDGE, EDGE),
+        )
+        .fill_paint(&radial)
+        .build()
+        .draw(&mut canvas);
+
+    canvas.pixmap
+}
